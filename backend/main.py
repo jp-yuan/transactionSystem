8fa780757a59ce64076e9c70e2146546dbbd3df0
jp-yuan <jianpengyuan@outlook.com>
2024-08-22 17:30:45 -0700

    working on firebase db

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,35 @@
-import pyrebase
+#import pyrebase
+from student import Student
 
-firebaseConfig = {
-    'apiKey': "AIzaSyC_jt407Uc3L6tB3rsT6_E8RUp8Z6Nnk1c",
-    'authDomain': "system-a28c4.firebaseapp.com",
-    'projectId': "system-a28c4",
-    'storageBucket': "system-a28c4.appspot.com",
-    'messagingSenderId': "563798016386",
-    'appId': "1:563798016386:web:7392d312a51f6966504f9b",
-    'measurementId': "G-HEJ0Z46R9W"
-}
+# firebaseConfig = {
+#     'apiKey': "AIzaSyC_jt407Uc3L6tB3rsT6_E8RUp8Z6Nnk1c",
+#     'authDomain': "system-a28c4.firebaseapp.com",
+#     'projectId': "system-a28c4",
+#     'storageBucket': "system-a28c4.appspot.com",
+#     'messagingSenderId': "563798016386",
+#     'appId': "1:563798016386:web:7392d312a51f6966504f9b",
+#     'measurementId': "G-HEJ0Z46R9W"
+# }
 
-firebase = pyrebase.initialize_app(firebaseConfig)
-auth = firebase.auth()
-db = firebase.database()
+# firebase = pyrebase.initialize_app(firebaseConfig)
+# auth = firebase.auth()
+# db = firebase.database()
 
-print("run")
+s1 = Student(
+    name="John Doe",
+    follow_up_note="Needs to improve math skills",
+    program="STEM",
+    counselor="Ms. Smith",
+    phone="555-1234",
+    email="john.doe@example.com",
+    grade="10",
+    school="High School ABC",
+    class_of="2025",
+    parent_name="Jane Doe",
+    parent_phone="555-5678",
+    parent_email1="jane.doe@example.com",
+    parent_email2="jane.doe@work.com"
+)
+
+
+print(s1.name)
--- backend/student.py
+++ backend/student.py
@@ -1,4 +1,3 @@
-
 class Student:
     def __init__(self, name, follow_up_note, 
                  program, counselor, phone, 
@@ -7,100 +6,123 @@ class Student:
                  parent_email1, parent_email2):
         
         self.name = name
-        self.follow_up_note = follow_up_note
-        self.program = program
-        self.counselor = counselor
-        self.phone = phone
-        self.email = email
-        self.grade = grade
-        self.school = school
-        self.class_of = class_of
-        self.parent_name = parent_name
-        self.parent_phone = parent_phone
-        self.parent_email1 = parent_email1
-        self.parent_email2 = parent_email2
+        self._follow_up_note = follow_up_note
+        self._program = program
+        self._counselor = counselor
+        self._phone = phone
+        self._email = email
+        self._grade = grade
+        self._school = school
+        self._class_of = class_of
+        self._parent_name = parent_name
+        self._parent_phone = parent_phone
+        self._parent_email1 = parent_email1
+        self._parent_email2 = parent_email2
 
-
-        # Getter and Setter for follow_up_note
-    def get_follow_up_note(self):
+    # Getter and Setter for follow_up_note
+    @property
+    def follow_up_note(self):
         return self._follow_up_note
 
-    def set_follow_up_note(self, follow_up_note):
-        self._follow_up_note = follow_up_note
+    @follow_up_note.setter
+    def follow_up_note(self, value):
+        self._follow_up_note = value
 
     # Getter and Setter for program
-    def get_program(self):
+    @property
+    def program(self):
         return self._program
 
-    def set_program(self, program):
-        self._program = program
+    @program.setter
+    def program(self, value):
+        self._program = value
 
     # Getter and Setter for counselor
-    def get_counselor(self):
+    @property
+    def counselor(self):
         return self._counselor
 
-    def set_counselor(self, counselor):
-        self._counselor = counselor
+    @counselor.setter
+    def counselor(self, value):
+        self._counselor = value
 
     # Getter and Setter for phone
-    def get_phone(self):
+    @property
+    def phone(self):
         return self._phone
 
-    def set_phone(self, phone):
-        self._phone = phone
+    @phone.setter
+    def phone(self, value):
+        self._phone = value
 
     # Getter and Setter for email
-    def get_email(self):
+    @property
+    def email(self):
         return self._email
 
-    def set_email(self, email):
-        self._email = email
+    @email.setter
+    def email(self, value):
+        self._email = value
 
     # Getter and Setter for grade
-    def get_grade(self):
+    @property
+    def grade(self):
         return self._grade
 
-    def set_grade(self, grade):
-        self._grade = grade
+    @grade.setter
+    def grade(self, value):
+        self._grade = value
 
     # Getter and Setter for school
-    def get_school(self):
+    @property
+    def school(self):
         return self._school
 
-    def set_school(self, school):
-        self._school = school
+    @school.setter
+    def school(self, value):
+        self._school = value
 
     # Getter and Setter for class_of
-    def get_class_of(self):
+    @property
+    def class_of(self):
         return self._class_of
 
-    def set_class_of(self, class_of):
-        self._class_of = class_of
+    @class_of.setter
+    def class_of(self, value):
+        self._class_of = value
 
     # Getter and Setter for parent_name
-    def get_parent_name(self):
+    @property
+    def parent_name(self):
         return self._parent_name
 
-    def set_parent_name(self, parent_name):
-        self._parent_name = parent_name
+    @parent_name.setter
+    def parent_name(self, value):
+        self._parent_name = value
 
     # Getter and Setter for parent_phone
-    def get_parent_phone(self):
+    @property
+    def parent_phone(self):
         return self._parent_phone
 
-    def set_parent_phone(self, parent_phone):
-        self._parent_phone = parent_phone
+    @parent_phone.setter
+    def parent_phone(self, value):
+        self._parent_phone = value
 
     # Getter and Setter for parent_email1
-    def get_parent_email1(self):
+    @property
+    def parent_email1(self):
         return self._parent_email1
 
-    def set_parent_email1(self, parent_email1):
-        self._parent_email1 = parent_email1
+    @parent_email1.setter
+    def parent_email1(self, value):
+        self._parent_email1 = value
 
     # Getter and Setter for parent_email2
-    def get_parent_email2(self):
+    @property
+    def parent_email2(self):
         return self._parent_email2
 
-    def set_parent_email2(self, parent_email2):
-        self._parent_email2 = parent_email2
+    @parent_email2.setter
+    def parent_email2(self, value):
+        self._parent_email2 = value
